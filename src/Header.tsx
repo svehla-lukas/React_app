dc4a70698f24634e8766adcd376fb7a306dfa084
Lukas Svehla <svehl.lukas@gmail.com>
2024-11-27 12:15:28 +0100

    chages to route

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,15 +1,12 @@
 import { Container } from './Styles'
 import { Link } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
 import Clock from './clock'
 
 const Header = () => {
-  const navigate = useNavigate()
-
   return (
     <Container flexDirection='row'>
       <div>
-        <Link to='/target-page'>
+        <Link to='/'>
           <img
             src={`${process.env.PUBLIC_URL}/logo.png`}
             alt='Navigate'
@@ -22,19 +19,19 @@ const Header = () => {
         <nav>
           <ul>
             <li>
-              <Link to='/gameTTT'>Tic tac toe</Link>
+              <Link to='gameTTT'>Tic tac toe</Link>
             </li>
             <li>
-              <Link to='/MemoryGame'>Memory game</Link>
+              <Link to='MemoryGame'>Memory game</Link>
             </li>
             <li>
-              <Link to='/HackerType'>HackerType</Link>
+              <Link to='HackerType'>HackerType</Link>
             </li>
             <li>
-              <Link to='/MapsCZ'>Maps CZ - API</Link>
+              <Link to='MapsCZ'>Maps CZ - API</Link>
             </li>
             <li>
-              <Link to='/Weather'>Weather - API</Link>
+              <Link to='Weather'>Weather - API</Link>
             </li>
           </ul>
         </nav>
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,4 @@
-// all - Import all members, where myModule contains all the exported bindings.
-
-// multiple - Import multiple members.
-
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Clock from './clock'
 import HackerType from './HackerType'
@@ -15,25 +11,20 @@ import TicTacToe from './gameTTT'
 import Weather from './Weather'
 import reportWebVitals from './reportWebVitals'
 
-// single - Import single member.
-
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <BrowserRouter basename='/React_app'>
     <Header />
     <Routes>
       <Route path='/' element={<HomePage />} />
-      <Route path='/clock' element={<Clock />} />
-      <Route path='/gameTTT' element={<TicTacToe />} />
-      <Route path='/MemoryGame' element={<MemoryGame />} />
-      <Route path='/HackerType' element={<HackerType />} />
-      <Route path='/Weather' element={<Weather />} />
-      <Route path='/MapsCZ' element={<MapsCZ />} />
+      <Route path='clock' element={<Clock />} />
+      <Route path='gameTTT' element={<TicTacToe />} />
+      <Route path='MemoryGame' element={<MemoryGame />} />
+      <Route path='HackerType' element={<HackerType />} />
+      <Route path='Weather' element={<Weather />} />
+      <Route path='MapsCZ' element={<MapsCZ />} />
     </Routes>
   </BrowserRouter>
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
