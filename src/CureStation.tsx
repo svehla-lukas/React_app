2ccb4b70a37bafb9704890f08a1120b9b7579689
Lukas Svehla <svehl.lukas@gmail.com>
2025-01-06 19:45:21 +0100

    add cureStation

---
+++ src/CureStation.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CureStation = () => {
+  return (
+    <div>
+      Cure station Git repository:<p></p> https://github.com/svehla-lukas/Cure_Station
+    </div>
+  )
+}
+export default CureStation
